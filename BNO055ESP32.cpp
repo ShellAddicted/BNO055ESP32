0866cd3ca0fcb70ce869eab00a216d8b598b9d07
ShellAddicted <ShellAddicted@gmail.com>
2024-02-17 12:58:15 +0100

    now compatible with idf v5.1.2

--- BNO055ESP32.cpp
+++ BNO055ESP32.cpp
@@ -170,7 +170,7 @@ void BNO055::uart_readLen(bno055_reg_t reg, uint8_t *buffer, uint8_t len, uint32
                 data = (uint8_t *)malloc(len + 2);
                 if (data == NULL) throw std::bad_alloc();  // malloc failed
             }
-            rxBytes = uart_read_bytes(_uartPort, data, (len + 2), timeoutMS / portTICK_RATE_MS);
+            rxBytes = uart_read_bytes(_uartPort, data, (len + 2), timeoutMS / portTICK_PERIOD_MS);
             if (rxBytes > 0) {
 #ifndef BNO055_DEBUG_OFF
                 ESP_LOGD(BNO055_LOG_TAG, "(RL) Read %d bytes", rxBytes);
@@ -231,7 +231,7 @@ void BNO055::uart_writeLen(bno055_reg_t reg, uint8_t *data2write, uint8_t len, u
 #endif
 
         if (timeoutMS > 0) {  // check response (if expected)
-            rxBytes = uart_read_bytes(_uartPort, data, 2, timeoutMS / portTICK_RATE_MS);
+            rxBytes = uart_read_bytes(_uartPort, data, 2, timeoutMS / portTICK_PERIOD_MS);
             if (rxBytes > 0) {
 #ifndef BNO055_DEBUG_OFF
                 ESP_LOGD(BNO055_LOG_TAG, "(WL) Read %d bytes", rxBytes);  // DEBUG
@@ -406,7 +406,7 @@ void BNO055::reset() {
 #ifndef BNO055_DEBUG_OFF
         ESP_LOGD(BNO055_LOG_TAG, "RST -> using hardware pin");  // DEBUG
 #endif
-        gpio_pad_select_gpio(_rstPin);
+        gpio_reset_pin(_rstPin);
         gpio_set_direction(_rstPin, GPIO_MODE_OUTPUT);
         gpio_set_level(_rstPin, 0);  // turn OFF
         vTaskDelay(1 / portTICK_PERIOD_MS);
@@ -790,7 +790,7 @@ void BNO055::begin() {
     }
 
     if (_intPin != GPIO_NUM_MAX) {
-        gpio_pad_select_gpio(_intPin);
+        gpio_reset_pin(_intPin);
         gpio_set_direction(_intPin, GPIO_MODE_INPUT);
         gpio_set_intr_type(_intPin, GPIO_INTR_POSEDGE);
         gpio_set_pull_mode(_intPin, GPIO_PULLDOWN_ONLY);
